a2c300af0cf3b3a2df6786a0ca11e2ee11397d65
Felipe Riboldi <feliperiboldimarques@outlook.com>
2018-12-01 19:31:44 -0200

    Variaveis

---
+++ variaveis.dart
@@ -0,0 +1,17 @@
+void main() {
+  // Implementando uma lojinha
+
+  String nome = "Lojinha do Dart";
+  int bananas = 5;
+  double preco = 10.50;
+  bool aindaTem = true;
+
+  bananas = 10;
+  print("O Nome da Lojinha é: " + nome);
+  print("A $nome tem $bananas bananas");
+
+  dynamic teste = 1;
+  teste = "Variavél Dinâmica";
+}
+
+
