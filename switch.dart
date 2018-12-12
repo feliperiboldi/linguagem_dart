bc4d18019f0d4417734a5cfc5e08d57ef22cebea
Felipe Riboldi <feliperiboldimarques@outlook.com>
2018-12-12 14:54:10 -0200

    Operador Condicional - Switch

---
+++ switch.dart
@@ -0,0 +1,23 @@
+void main() {
+  
+	String linguagem = "PHP";
+  
+  switch(linguagem) {
+    case "Dart":
+      print("Dart");
+      break;
+     
+    case "Java":
+      print("Java");
+      break;
+      
+    case "Swift":
+      print("Swift");
+      break;
+      
+    default:
+      print("Outra");
+      break;
+  }
+  
+}
